17e9b9a80678c2957e5edd4ae707aa10ba328070
Osama <osama.salahuddin@gmail.com>
2025-04-08 00:52:30 +0200

    - networkTimeManager class added

---
+++ app/src/networkTimeManager/networkTimeManager.cpp
@@ -0,0 +1,109 @@
+// app/src/net/networkTimeManager.cpp
+#include "networkTimeManager.hpp"
+
+#include "myLogger.h"
+
+#include <zephyr/net/sntp.h>
+#include <zephyr/net/net_ip.h>
+#include <zephyr/posix/time.h>
+#include <zephyr/kernel.h>
+#include <cstring>
+
+static networkTimeManager* instance_ptr = nullptr;
+
+networkTimeManager& networkTimeManager::getInstance()
+{
+    if (!instance_ptr)
+    {
+        static networkTimeManager inst;
+        instance_ptr = &inst;
+    }
+    return *instance_ptr;
+}
+
+void networkTimeManager::init()
+{
+
+
+}
+
+void convert_unix_time_to_date(uint64_t unix_time)
+{
+    struct tm time_info;
+
+    /* Convert Unix time to UTC (or local time if using localtime_r) */
+    gmtime_r((time_t*)&unix_time, &time_info);
+
+    MYLOG("Converted Time (UTC): %d-%02d-%02d %02d:%02d:%02d",
+          (time_info.tm_year + 1970), (time_info.tm_mon + 1), (time_info.tm_mday - 1),
+          time_info.tm_hour, time_info.tm_min, time_info.tm_sec);
+}
+
+bool networkTimeManager::sync(const char* server, int timeout_ms)
+{
+    struct sntp_ctx ctx = {};
+    struct sntp_time sntp_time = {};
+    int ret;
+    bool return_value = false;
+
+
+    std::string server_ = "5.9.19.62";
+    server = server_.c_str();
+
+    synced = false;
+
+    /* ipv4 */
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(SNTP_PORT);
+
+    ret = inet_pton(AF_INET, server_.c_str(), &addr.sin_addr);
+    if (ret != 1)
+    {
+        MYLOG("Failed to convert address %d", ret);
+    }
+
+    ret = sntp_init(&ctx, (struct sockaddr *) &addr,sizeof(struct sockaddr_in));
+    if (ret < 0)
+    {
+        MYLOG("Failed to init SNTP: %d", ret);
+    }
+    else
+    {
+        ret = sntp_query(&ctx, timeout_ms, &sntp_time);
+
+        if (ret < 0)
+        {
+            MYLOG("SNTP request failed: %d", ret);
+            sntp_close(&ctx);
+            return false;
+        }
+        else
+        {
+            struct timespec ts;
+            ts.tv_sec = sntp_time.seconds;
+            ts.tv_nsec = ((uint64_t)sntp_time.fraction * NSEC_PER_SEC) >> 32;
+
+            /* Convert NTP epoch to Unix epoch */
+            uint64_t unix_time = sntp_time.seconds - 2208988800ULL;
+
+            /* Convert the NTP time to a human-readable format */
+            convert_unix_time_to_date(unix_time);
+
+            clock_settime(CLOCK_REALTIME, &ts);
+
+            sntp_close(&ctx);
+            synced = true;
+            return_value = true;
+        }
+    }
+
+    return return_value;
+}
+
+bool networkTimeManager::is_synced() const
+{
+    return synced;
+}
